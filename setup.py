3953f0367a570169be80386afb5a404d8be941de
cangers <corey.angers@gmail.com>
2018-06-03 18:05:22 -0400

    begin project
    set up directory structure and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SqlTable',
+    version='1.0',
+    packages=['conf'],
+    url='www.not-a-site.com',
+    license='None',
+    author='Corey',
+    author_email='corey.angers@gmail.com',
+    description='Tool for interfacing with relational databases'
+)
---
+++ sqltable/conf/__init__.py
@@ -0,0 +1,4 @@
+import yaml
+
+with open('databases.yaml', 'r') as yml:
+    databases = yaml.load(yml)
